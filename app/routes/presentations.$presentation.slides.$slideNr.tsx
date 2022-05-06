c54ab5b44245c3700776dde0d8a4219af15d7a14
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2022-05-06 23:20:39 +0200

    SlideNr route without wrapping the route's components

---
+++ app/routes/presentations.$presentation.slides.$slideNr.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react'
+import { LoaderFunction, useLoaderData } from 'remix'
+import { getMdx } from '~/server/mdx.server'
+import { getSlides } from '~/server/getSlides.server'
+import { useNavigate } from 'react-router-dom'
+import useKeyboardNavigation from '~/client/useKeyboardNavigation'
+import Fullscreen from '~/components/slide/Fullscreen'
+import { Slide } from '~/components/slide/Slide'
+
+export type LoaderData = {
+  slide: {
+    code: string
+    frontmatter: {
+      [key: string]: any
+    }
+    previousSlideNr?: number
+    nextSlideNr?: number
+  }
+  numberOfSlides: number
+  presentationSlug: string
+}
+
+export const loader: LoaderFunction = async ({ params }): Promise<LoaderData> => {
+  const presentationSlug = params.presentation ?? ''
+  const slideNr = parseInt(params.slideNr ?? '1')
+
+  const slides = await getSlides(presentationSlug)
+
+  const slide = slides.find((slide) => slide.nr === slideNr)
+
+  if (!slide) {
+    throw new Response(`Not Found: slideNr ${slideNr} could not be found`, {
+      status: 404,
+    })
+  }
+
+  const mdx = await getMdx(slide.mdxContent)
+
+  return { slide: { ...slide, ...mdx }, numberOfSlides: slides.length, presentationSlug }
+}
+
+export default () => {
+  const {
+    slide: { code, previousSlideNr, nextSlideNr },
+    presentationSlug,
+  } = useLoaderData<LoaderData>()
+
+  const navigate = useNavigate()
+
+  const goToNextSlide = React.useCallback(() => {
+    if (!nextSlideNr) return
+    navigate(`/presentations/${presentationSlug}/slides/${nextSlideNr}`)
+  }, [navigate, nextSlideNr, presentationSlug])
+  const goToPreviousSlide = React.useCallback(() => {
+    if (!previousSlideNr) return
+    navigate(`/presentations/${presentationSlug}/slides/${previousSlideNr}`)
+  }, [navigate, previousSlideNr, presentationSlug])
+
+  useKeyboardNavigation(goToNextSlide, goToPreviousSlide)
+
+  return (
+    <Fullscreen>
+      <Slide code={code} />
+    </Fullscreen>
+  )
+}
