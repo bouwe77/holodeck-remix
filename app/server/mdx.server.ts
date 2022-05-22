8d22514d6661c51d15298ecb42c70b67d9e4f9a5
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2022-05-22 23:02:48 +0200

    feat: First iteration of reading all TSX components and passing them to MDX bundler

--- app/server/mdx.server.ts
+++ app/server/mdx.server.ts
@@ -5,17 +5,42 @@ import { bundleMDX } from 'mdx-bundler'
 const componentsFolder = Path.resolve(__dirname, '../../app/client/slides/layout')
 const kutzooi = Path.resolve(__dirname, '../../slides/example-presentation')
 
+const getComponentFilenames = async (folder: string) => {
+  const files = await fs
+    .readdir(folder, { withFileTypes: true })
+    .then((files) => files.filter((file) => file.isFile() && Path.extname(file.name) === '.tsx'))
+    .then((files) => files.map((file) => file.name))
+
+  return files
+}
+
+const getComponentContents = async (folder: string, filenames: string[]) => {
+  const stuff = await Promise.all(
+    filenames.map(async (file) => {
+      const filePath = Path.join(folder, file)
+      const fileContents = await fs.readFile(filePath, 'utf8')
+      return {
+        [`./${file}`]: fileContents,
+      }
+    }),
+  )
+
+  return stuff.reduce((acc, curr) => ({ ...acc, ...curr }), {})
+}
+
 export const getMdx = async (mdxSourceCode: string) => {
-  const center = await fs.readFile(Path.join(componentsFolder, 'Center.tsx'), 'utf8')
-  const left = await fs.readFile(Path.join(componentsFolder, 'Left.tsx'), 'utf8')
-  const counter = await fs.readFile(Path.join(kutzooi, 'Counter.tsx'), 'utf8')
+  const componentFilenames = await getComponentFilenames(componentsFolder)
+  const componentFilenames2 = await getComponentFilenames(kutzooi)
+
+  // create prut object: key is componentFilenames, file contents is value
+  const prut = await getComponentContents(componentsFolder, componentFilenames)
+  const prut2 = await getComponentContents(kutzooi, componentFilenames2)
 
   const result = await bundleMDX({
     source: mdxSourceCode,
     files: {
-      './Center.tsx': center,
-      './Left.tsx': left,
-      './Counter.tsx': counter,
+      ...prut,
+      ...prut2,
     },
   })
 
