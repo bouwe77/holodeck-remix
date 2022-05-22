7b37bf46ff7b4845bce0c561af75b85316de2987
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2022-05-22 22:40:29 +0200

    feat: Add Counter component

--- app/server/mdx.server.ts
+++ app/server/mdx.server.ts
@@ -3,16 +3,19 @@ import Path from 'path'
 import { bundleMDX } from 'mdx-bundler'
 
 const componentsFolder = Path.resolve(__dirname, '../../app/client/slides/layout')
+const kutzooi = Path.resolve(__dirname, '../../slides/example-presentation')
 
 export const getMdx = async (mdxSourceCode: string) => {
   const center = await fs.readFile(Path.join(componentsFolder, 'Center.tsx'), 'utf8')
   const left = await fs.readFile(Path.join(componentsFolder, 'Left.tsx'), 'utf8')
+  const counter = await fs.readFile(Path.join(kutzooi, 'Counter.tsx'), 'utf8')
 
   const result = await bundleMDX({
     source: mdxSourceCode,
     files: {
       './Center.tsx': center,
       './Left.tsx': left,
+      './Counter.tsx': counter,
     },
   })
 
