1b46dae0618264e0f9ecf529efb50ca24bf0751a
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2022-05-29 11:39:31 +0200

    chore: update all packages

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,8 @@
 import { RemixBrowser } from '@remix-run/react'
+
+// Should use hydrateRoot for React 18, but this has some issues still,
+// so by using hydrate the app will still behave as if it's running React 17
+
 import { hydrate } from 'react-dom'
 
 hydrate(<RemixBrowser />, document)
